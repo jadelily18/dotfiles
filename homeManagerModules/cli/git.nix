16fe741211ea7b2044ab4bf84ae6c13ad3bd7e44
Jade Nash <jade@lilydev.com>
2025-02-05 10:46:17 -0600

    Maybe fix git signing part 91395983180831508

--- homeManagerModules/cli/git.nix
+++ homeManagerModules/cli/git.nix
@@ -16,16 +16,25 @@ in
   config = lib.mkIf cfg.enable {
     programs.git = {
       enable = true;
+
+      userName = "Jade Nash";
+      userEmail = "jade@lilydev.com";
+
+      signing = {
+        key = "910F4FE160AE36BA";
+        signByDefault = true;
+      };
+
       extraConfig = {
         credential.helper = "${pkgs.git.override { withLibsecret = true; }}/bin/git-credential-libsecret";
 
         init.defaultBranch = "main";
 
-        user = {
-          signingKey = "910F4FE160AE36BA";
-          name = "Jade Nash";
-          email = "jade@lilydev.com";
-        };
+        # user = {
+        #   signingKey = "910F4FE160AE36BA";
+        #   name = "Jade Nash";
+        #   email = "jade@lilydev.com";
+        # };
       };
     };
   };
