85a87ce148680552549463e2995753eec87f8d12
Jade Nash <jade@lilydev.com>
2025-02-06 15:27:40 -0600

    Update starship config

--- modules/home/cli/starship.nix
+++ modules/home/cli/starship.nix
@@ -17,23 +17,23 @@ in
         format = "$username$hostname $directory $character";
 
         character = {
-          success_symbol = "[>>](bold green)";
-          error_symbol = "[??](bold red)";
+          success_symbol = "[\\$](bold #f2cdcd)";
+          error_symbol = "[?](bold #f38ba8)";
         };
 
         username = {
           show_always = true;
-          format = "[$user]($style italic)";
+          format = "[$user](bold italic #f5c2e7)";
           disabled = false;
         };
 
         hostname = {
           ssh_only = true;
-          format = "[@$hostname]($style italic purple)";
+          format = "[@$hostname](bold italic #cba6f7)";
         };
 
         directory = {
-          format = "[$path]($style italic)";
+          format = "[$path]($style italic #b4befe)";
         };
       };
     };
