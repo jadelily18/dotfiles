f7b74776ec7d1eb67a82c1a183ee7782ca05514b
Jade Nash <jade@lilydev.com>
2025-02-18 21:36:19 -0600

    chore(flake): Update everything + add heroic launcher for desktop

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -45,6 +45,7 @@
       # davinci-resolve doesn't work properly and I don't want to fix it right now
       spotify
       spicetify-cli
+      heroic
     ]
     ++ (import ../../modules/home/shared/packages.nix { inherit pkgs; })
     ++ (import ../../modules/home/shared/gnomeExtensions.nix { inherit pkgs; })
